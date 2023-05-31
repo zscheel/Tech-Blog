5e5d3686c3a627abaa3667965c46ec7e11c54989
zscheel <zachary.scheel1@gmail.com>
2023-05-30 22:32:32 -0500

    added code to the server js file and sync sequlize

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const routes = require('./controllers');
+const sequelize = require('./config/connection');
+const path = require('path');
+const exphbs = require('express-handlebars');
+const helpers = require('./utils/helpers');
+const hbs = exphbs.create({ helpers });
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+// import sequelize connection
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const sess = {
+  secret: 'secret secret code',
+  cookie: {
+    maxAge: 1*60*60*1000
+  },
+  resave: false,
+  saveUnitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+}
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(session(sess));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));;
+
+app.use(routes);
+
+// sync sequelize models to the database, then turn on the server
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
+});
+
